627727d0e4866dc4957ee3aa59eeeefa313225b0
Sulthon Aufa <sulthonhere@gmail.com>
2024-03-01 14:34:34 +0700

    rm duplicate tb_instansi

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -36,10 +35,10 @@ return new class extends Migration
             $table->string('foto')->nullable();
             $table->text('alamat')->nullable();
             $table->string('alamat_kota')->nullable();
-            $table->char('jenis_kelamin', 1); 
+            $table->char('jenis_kelamin', 1);
             $table->string('no_telp')->nullable();
             $table->string('role');
-            $table->timestamps(); 
+            $table->timestamps();
 
             // Foreign key 
             $table->unsignedBigInteger('id_instansi');
