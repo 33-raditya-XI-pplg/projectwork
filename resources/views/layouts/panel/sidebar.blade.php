949cd3034e5c5f935dd68c553292978217ebd095
Miftahus Surur Sutowo <62152669+miftahozuki@users.noreply.github.com>
2024-03-15 15:17:34 +0700

    fix button sidebar

--- resources/views/layouts/panel/sidebar.blade.php
+++ resources/views/layouts/panel/sidebar.blade.php
@@ -86,8 +86,10 @@
 </aside>
 
 @push('script')
-    <Script>
-        // Fungsi untuk menangani klik tombol sidebar
+<script>
+    // Fungsi untuk menangani klik tombol sidebar
+    var sidebarState = false; // Menyimpan status sidebar terbuka atau tertutup
+
     document.getElementById("btn-show-sidebar").addEventListener("click", function() {
         // Mengambil semua elemen <span> di dalam sidebar
         var spans = document.querySelectorAll('.sidebar-menu span');
@@ -95,13 +97,27 @@
         var sidebar = document.getElementById('panel-sidebar');
         var button = document.getElementById('btn-show-sidebar');
 
-        // Melooping semua elemen <span> dan menghapus teksnya
-        spans.forEach(function(span) {
-            span.textContent = ''; // Menghilangkan teks
-            logo.style.visibility = 'hidden';
-            sidebar.style.maxWidth = '90px';
-            button.style.left = '50%';
-        });
+        // Jika sidebar sedang terbuka
+        if (sidebarState) {
+            // Melooping semua elemen <span> dan mengembalikan teksnya
+            spans.forEach(function(span) {
+                span.style.display = ''; // Mengembalikan teks
+            });
+            logo.style.visibility = 'visible'; // Menampilkan kembali logo
+            sidebar.style.maxWidth = '260px'; // Mengembalikan lebar sidebar ke kondisi semula
+            button.style.left = '85%'; // Mengembalikan posisi tombol
+            sidebarState = false; // Menandakan sidebar kembali tertutup
+        } else { // Jika sidebar sedang tertutup
+            // Melooping semua elemen <span> dan menghapus teksnya
+            spans.forEach(function(span) {
+                span.style.display = 'none'; // Menghilangkan teks
+            });
+            logo.style.visibility = 'hidden'; // Menyembunyikan logo
+            sidebar.style.maxWidth = '90px'; // Mengubah lebar sidebar
+            button.style.left = '50%'; // Mengubah posisi tombol
+            sidebarState = true; // Menandakan sidebar sedang terbuka
+        }
     });
-    </Script>
+</script>
+
 @endpush
