3a5bf49f82979addf2c86ec899036063aba46ed4
Sulthon Aufa <sulthonhere@gmail.com>
2024-03-19 13:48:41 +0700

    Update 2024_03_18_000005_tb_instansi.php

--- database/migrations/2024_03_18_000005_tb_instansi.php
+++ database/migrations/2024_03_18_000005_tb_instansi.php
@@ -13,12 +13,6 @@ return new class extends Migration
     {
         Schema::create('tb_instansi', function (Blueprint $table) {
             $table->id('id_instansi');
-            
-            $table->string('created_by');
-            $table->date('created_at');
-
-            $table->string('updated_by');
-            $table->date('updated_at');
 
             $table->string('nama_instansi');
             $table->integer('nomor_instansi');
@@ -26,15 +20,18 @@ return new class extends Migration
             $table->string('nama_kepala_instansi');
             $table->string('jabatan_kepala');
 
-            $table->text('path_logo');
+            $table->string('path_logo');
             $table->string('status');
 
             $table->text('alamat');
             $table->string('alamat_kota');
             $table->string('email')->unique();
             $table->integer('no_telp')->unique();
-
-            // $table->timestamps();
+            
+            $table->string('created_by');
+            $table->string('updated_by');
+            
+            $table->timestamps();
         });
     }
 
