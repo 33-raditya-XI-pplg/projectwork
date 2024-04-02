e61e973aa6cebdadeeb70f615d6ff141c5b7760d
Miftahus Surur Sutowo <62152669+miftahozuki@users.noreply.github.com>
2024-04-02 10:54:29 +0700

    membuat tampilan dashboard

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -2,4 +2,101 @@
 @section('title', 'Dashboard')
 @section('content')
 
+    <div class="container">
+        <div class="card bg-primary-gradient text-white rounded-3 shadow-lg" style="height: 7rem;">
+            <div class="card-body d-flex align-items-center justify-content-between mx-3">
+                <div>
+                    <h5 class="card-title">Hi, <strong>{{ Auth::user()->nama_lengkap }}</strong></h5>
+                    <p class="card-text">Selamat datang dan selamat bekerja!</p>
+                </div>
+                <div>
+
+                    <div class="input-group mb-3">
+                        <span class="input-group-text"><i class="fa-solid fa-magnifying-glass"></i></span>
+                        <input type="number" class="form-control"
+                            aria-label="Dollar amount (with dot and two decimal places)" name="biaya_regis"
+                            placeholder="Search">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-3">
+                <div class="card mt-4 bg-primary-gradient text-white rounded-3 shadow-sm"
+                    style="width: 15rem; height: 8rem">
+                    <div class="card-body">
+                        <div class="position-absolute top-0 end-0 mt-2 me-2">
+                            <i class="fa-solid fa-users fs-1"></i>
+                        </div>
+                        <div class="position-absolute top-0 end-0 mt-2 me-2">
+                            <i class="fa-solid fa-users fs-1"></i>
+                        </div>
+                        <h1 class="card-title fw-bold">{{ rand(1, 100) }}</h1>
+                        <p class="card-text fw-bold">Total Pengguna</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="card mt-4 bg-primary-gradient text-white rounded-3 shadow-sm"
+                    style="width: 15rem; height: 8rem">
+                    <div class="card-body">
+                        <div class="position-absolute top-0 end-0 mt-2 me-2">
+                            <i class="fa-solid fa-users fs-1"></i>
+                        </div>
+                        <h1 class="card-title fw-bold">{{ rand(1, 100) }}</h1>
+                        <p class="card-text fw-bold">Total Pengguna</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="card mt-4 bg-primary-gradient text-white rounded-3 shadow-sm"
+                    style="width: 15rem; height: 8rem">
+                    <div class="card-body">
+                        <div class="position-absolute top-0 end-0 mt-2 me-2">
+                            <i class="fa-solid fa-users fs-1"></i>
+                        </div>
+                        <h1 class="card-title fw-bold">{{ rand(1, 100) }}</h1>
+                        <p class="card-text fw-bold">Total Pengguna</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="card mt-4 bg-primary-gradient text-white rounded-3 shadow-sm"
+                    style="width: 15rem; height: 8rem">
+                    <div class="card-body">
+                        <div class="position-absolute top-0 end-0 mt-2 me-2">
+                            <i class="fa-solid fa-users fs-1"></i>
+                        </div>
+                        <h1 class="card-title fw-bold">{{ rand(1, 100) }}</h1>
+                        <p class="card-text fw-bold">Total Pengguna</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="row mt-4">
+            <div class="col-8 bg-white text-center">
+                <div class="card shadow-md rounded-3" style="height: 12rem;">
+                    <div class="card-body">
+                        {{-- This is some text within a card body. --}}
+                        <div class="spinner-border text-primary" role="status">
+                            <span class="visually-hidden">Loading...</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-4 bg-white text-center">
+                <div class="card shadow-md rounded-3" style="height: 12rem;">
+                    <div class="card-body">
+                        {{-- This is some text within a card body. --}}
+                        <div class="spinner-border text-primary" role="status">
+                            <span class="visually-hidden">Loading...</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
 @endsection
