a20311df7b6b1e45c65d9013a15fe45d9d6f865a
Sulthon Aufa <sulthonhere@gmail.com>
2024-04-18 11:46:59 +0700

    minor update: data-seeders

--- database/seeders/SkemaSeeder.php
+++ database/seeders/SkemaSeeder.php
@@ -45,6 +45,22 @@ class SkemaSeeder extends Seeder
                 'status' => 'Aktif',
 
                 'created_by' => 0,
+            ],
+            [
+                'nama_skema' => 'UKK Jurusan RPL',
+                'path_icon' => 'assets\img\dummy\icon_skema\icon_skema_2.png',
+                'has_sub_skema' => 1,
+                'status' => 'Aktif',
+
+                'created_by' => 0,
+            ],
+            [
+                'nama_skema' => 'UKK Jurusan TKJ',
+                'path_icon' => 'assets\img\dummy\icon_skema\icon_skema_2.png',
+                'has_sub_skema' => 1,
+                'status' => 'Aktif',
+
+                'created_by' => 0,
             ]
         ]); 
 
@@ -94,6 +110,30 @@ class SkemaSeeder extends Seeder
             [
                 'skema_id' => 4,
                 'judul_sub' => 'Mampu membuat artikel pada microsoft word',
+
+                'created_by' => 0,
+            ],
+            [
+                'skema_id' => 5,
+                'judul_sub' => 'Memahami Konsep DSA dan CRUD dalam suatu website',
+
+                'created_by' => 0,
+            ],
+            [
+                'skema_id' => 5,
+                'judul_sub' => 'Mampu membuat CRUD website menggunakan php native',
+
+                'created_by' => 0,
+            ],
+            [
+                'skema_id' => 6,
+                'judul_sub' => 'Memahami Konsep Internet Protocol dan Routing jaringan',
+
+                'created_by' => 0,
+            ],
+            [
+                'skema_id' => 6,
+                'judul_sub' => 'Mampu mensimulasikan konsep Vlan dalam Packet-Tracer',
 
                 'created_by' => 0,
             ]
