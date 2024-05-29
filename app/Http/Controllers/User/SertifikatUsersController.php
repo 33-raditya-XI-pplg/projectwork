38b8165eefdbfc0f67f93dc7a14ca469b6b61058
Sulthon Aufa <sulthonhere@gmail.com>
2024-05-29 09:27:05 +0700

    Update SertifikatUsersController.php

--- app/Http/Controllers/User/SertifikatUsersController.php
+++ app/Http/Controllers/User/SertifikatUsersController.php
@@ -13,15 +13,15 @@ class SertifikatUsersController extends Controller
 {
     public function index()
     {
-        $data_sertifikat_peserta= DB::table('tb_peserta')
+    $data_sertifikat_peserta = DB::table('tb_peserta')
                         ->join('tb_event_skema', 'tb_peserta.event_skema_id', '=', 'tb_event_skema.id_event_skema')
                         ->join('tb_event', 'tb_event_skema.event_id', '=', 'tb_event.id_event')
                         ->join('tb_skema', 'tb_event_skema.skema_id', '=', 'tb_skema.id_skema')
-                        ->select('tb_event.id_event', 'tb_event.nama_event', 'tb_event.status',
-                                 'tb_event.tgl_mulai', 'tb_event.tgl_berakhir',
+                        ->select('tb_event.id_event', 'tb_event.nama_event', 'tb_event.status', 
+                                 'tb_event.tgl_mulai', 'tb_event.tgl_berakhir'
                         )
                         ->where('tb_peserta.user_id', Auth::user()->id_user)
-                        ->groupBy('tb_event.id_event', 'tb_event.nama_event', 'tb_event.status')
+                        ->distinct()
                         ->get();
 
         return view('user.sertifikat.index', compact('data_sertifikat_peserta'));
