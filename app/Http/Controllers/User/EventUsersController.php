3577be8ab3f6d2e1084aaa9607e460a6ee10b2d0
33-raditya-XI-pplg <raditekapranata@gmail.com>
2025-07-09 23:02:46 +0700

    update

--- app/Http/Controllers/User/EventUsersController.php
+++ app/Http/Controllers/User/EventUsersController.php
@@ -40,7 +40,7 @@ class EventUsersController extends Controller
 
         $data_event = $query
             ->where('tb_event.visibilitas', 'publik')
-            ->whereIn('tb_event.status', ['Berlangsung', 'Publish'])
+            ->whereIn('tb_event.status', ['Publish'])
             ->join('tb_jenis_event', 'tb_event.jenis_event_id', '=', 'tb_jenis_event.id_jenis_event')
             ->join('tb_tempat', 'tb_event.tempat_id', '=', 'tb_tempat.id_tempat')
             ->paginate(9);
