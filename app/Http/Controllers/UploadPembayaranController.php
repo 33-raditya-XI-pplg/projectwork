35d222e79dfd2573e2b195f5f07fd636b827c071
Mascitra.com <157489886+mascitradotcom@users.noreply.github.com>
2025-07-11 10:26:24 +0700

    Update UploadPembayaranController.php

--- app/Http/Controllers/UploadPembayaranController.php
+++ app/Http/Controllers/UploadPembayaranController.php
@@ -70,8 +70,8 @@ class UploadPembayaranController extends Controller
                     'tb_event.tgl_berakhir',
                     'tb_event.status'
                 )
-                ->groupBy('tb_event_skema.id_event_skema', 'tb_event_skema.event_id', 'tb_upload_pembayaran.bukti_pembayaran')
                 ->orderByDesc('tb_upload_pembayaran.id_upload_pembayaran')
+                ->limit(1)
                 ->get();
 
             $Title = 'Upload Pembayaran';
